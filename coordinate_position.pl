ceaf5ce78dccc8cef1d57aba0bef309d971ac5cc
-IvanWoo- <33291886+IvanWoo22@users.noreply.github.com>
2019-09-07 23:38:55 +0800

    Update coordinate_position.pl

--- coordinate_position.pl
+++ coordinate_position.pl
@@ -15,7 +15,7 @@ my %trans_dir;
 while(<STDIN>){
     chomp;
     my ($chr, $start, $end, $dir, $info) = split(/\t/,$_);
-    $info =~ /Parent=transcript:([A-Z,a-z,0-9]+])/;
+    $info =~ /Parent=transcript:([A-Z,a-z,0-9]+)/;
     if(exists($trans_chr{$1})){
         $trans_range{$1}->add_range($start, $end);
     }else{
@@ -26,12 +26,33 @@ while(<STDIN>){
     }
 }
 
-my $index = "ENST00000450305";
-my $site = "20";
-if($trans_dir{$index} eq "+"){
-    my $island = $trans_range{$index}->at($site);
-    print("$trans_chr{$index}\t$island\n");
-}else{
-    my $island = $trans_range{$index}->at(-$site);
-    print("$trans_chr{$index}\t$island\n");
+sub COORDINATE_POS{
+    my $index = shift;
+    my $site = shift;
+    my $island;
+    if($trans_dir{$index} eq "+"){
+        $island = $trans_range{$index}->at($site);
+    }else{
+        $island = $trans_range{$index}->at(-$site);
+    }
+    my $abs_site = $trans_chr{$index}."\t".$island;
+    return($abs_site);
 }
+
+open(IN_SAM, "<", $ARGV[0]);
+
+my %exist;
+while(<IN_SAM>){
+    chomp;
+    my ($read_name, $trans_info, $site) = split(/\t/, $_, 3);
+    $trans_info =~ /(ENST[0-9]+)/;
+    my $abs_site = COORDINATE_POS($1, $site);
+    my $read_abs_site = $read_name."\t".$abs_site;
+    unless(exists($exist{$read_abs_site}){
+        $exist{$read_abs_site} = 1;
+        print("$_\n");
+    }
+}
+close(IN_SAM);
+
+__END__
