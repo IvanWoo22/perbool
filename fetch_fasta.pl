d23e05df6bcfa596391bdbde397ae1a411672227
-IvanWoo- <33291886+IvanWoo22@users.noreply.github.com>
2019-09-28 15:47:58 +0800

    Update fetch_fasta.pl

--- fetch_fasta.pl
+++ fetch_fasta.pl
@@ -2,27 +2,60 @@
 use strict;
 use warnings;
 
-##########################
-# TODO: 2019/9/4 7:59 PM Add Get Option
-##########################
+use Getopt::Long;
 
-my $char = $ARGV[0];
-open FA, "<", $ARGV[1];
-my @info = <FA>;
-close FA;
+#---------------#
+# GetOpt section
+#---------------#
+
+=head1 NAME
+
+fetch_fasta.pl -- Get all sequences with the same searched string in a FastA file.
+
+=head1 SYNOPSIS
+
+perl fetch_fasta.pl <-s string> [options]
+    Options:
+        --help\-h   Brief help message.
+        --string\-s    The sequences we want to fetch.
+        --file\-f   The FastA file with path.
+        --stdin  Get FastA from STDIN. It will not been not valid with a provided '--file'.
+
+=cut
+
+Getopt::Long::GetOptions(
+    'help|h'     => sub { Getopt::Long::HelpMessage(0) },
+    'string|s=s' => \my $char,
+    'file|f=s'   => \my $in_fa,
+    'stdin'      => \my $stdin,
+) or Getopt::Long::HelpMessage(1);
+
+my @info;
+if ( defined($in_fa) ) {
+    open FA, "<", $in_fa or die("$in_fa: $!");
+    @info = <FA>;
+    close FA;
+}
+elsif ( defined($stdin) ) {
+    @info = <>;
+}
+else {
+    die("You should provide FastA!");
+}
 
 my $i = 0;
-while ($i <= $#info) {
-    if ($info[$i] =~ /$char/) {
-        print($info[$i]);
+while ( $i <= $#info ) {
+    if ( $info[$i] =~ /$char/ ) {
+        print( $info[$i] );
         my $j = 1;
-        until ($info[$i+$j] =~ /^>/) {
-            print($info[$i+$j]);
-            $j ++ ;
+        until ( $info[ $i + $j ] =~ /^>/ ) {
+            print( $info[ $i + $j ] );
+            $j++;
         }
         $i += $j;
-    }else{
-        $i ++;
+    }
+    else {
+        $i++;
     }
 }
 
