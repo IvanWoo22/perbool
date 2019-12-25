9867800e6519d03440c690eebfba9d4a2c8c96f0
-IvanWoo- <33291886+IvanWoo22@users.noreply.github.com>
2019-12-25 16:37:12 +0800

    Update count_duplication.pl

--- count_duplication.pl
+++ count_duplication.pl
@@ -12,10 +12,12 @@ while (<STDIN>) {
     }
 }
 
-my ($t1,$t5,$t10,$t50,$t100,$tm)=(0,0,0,0,0,0);
+my ($t1,$t3,$t5,$t10,$t50,$t100,$t300,$tm)=(0,0,0,0,0,0,0,0);
 foreach my $k (keys(%list)){
     if ( $list{$k}==1){
         $t1++;
+    }elsif($list{$k}<=3){
+        $t3++;
     }elsif($list{$k}<=5){
         $t5++;
     }elsif($list{$k}<=10){
@@ -24,15 +26,19 @@ foreach my $k (keys(%list)){
         $t50++;
     }elsif($list{$k}<=100){
         $t100++;
-    }elsif($list{$k}>100){
+    }elsif($list{$k}<=300){
+        $t300++;
+    }elsif($list{$k}>300){
         $tm++;
     }
 }
 print("1:\t$t1\n");
-print("2-5:\t$t5\n");
+print("2-3:\t$t3\n");
+print("4-5:\t$t5\n");
 print("6-10:\t$t10\n");
 print("11-50:\t$t50\n");
 print("51-100:\t$t100\n");
-print(">100:\t$tm\n");
+print("101-300:\t$t300\n");
+print(">300:\t$tm\n");
 
 __END__
