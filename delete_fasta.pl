f787623cd8aa02cc15de9d13084d28f350c86006
-IvanWoo- <33291886+IvanWoo22@users.noreply.github.com>
2020-01-09 09:40:26 +0800

    Create delete_fasta.pl

---
+++ delete_fasta.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+open FA, "<", $ARGV[0] or die("$ARGV[0]: $!");
+my @info = <FA>;
+
+my @list;
+open LS, "<", $ARGV[1] or die("$ARGV[1]: $!");
+while (<LS>) {
+    chomp;
+    push( @list, $_ );
+}
+
+open OUT, ">", $ARGV[2];
+foreach my $char (@list) {
+    my $i = 0;
+    my $j = 1;
+    while ( $i <= $#info ) {
+        if ( $info[$i] =~ /$char/ ) {
+            print OUT "$info[$i]";
+            until ( $info[ $i + $j ] =~ /^>/ ) {
+                print OUT "$info[$i+$j]";
+                $j++;
+            }
+            last;
+        }
+        else {
+            $i++;
+        }
+    }
+    splice @info, $i, $j;
+}
+
+foreach (@info) {
+    print("$_\n");
+}
+
+__END__
