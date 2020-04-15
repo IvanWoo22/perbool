f353a49fd4b246de54e31ce196c397dd2e573a5c
-IvanWoo- <33291886+IvanWoo22@users.noreply.github.com>
2020-04-15 12:25:04 +0800

    Update fetch_fasta.pl

--- fetch_fasta.pl
+++ fetch_fasta.pl
@@ -1,26 +1,19 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 use strict;
 use warnings;
 
 use Getopt::Long;
 
-#---------------#
-# GetOpt section
-#---------------#
-
 =head1 NAME
-
 fetch_fasta.pl -- Get all sequences with the same searched string in a FastA file.
-
 =head1 SYNOPSIS
-
     perl fetch_fasta.pl -s protein_coding [options]
         Options:
             --help\-h   Brief help message
             --string\-s The sequences we want to fetch
             --file\-f   The FastA file with path
             --stdin     Get FastA from STDIN. It will not been not valid with a provided '--file'
-
+            --rna2dna   Change "U" to "T". Default: False
 =cut
 
 Getopt::Long::GetOptions(
@@ -28,11 +21,17 @@ Getopt::Long::GetOptions(
     'string|s=s' => \my $char,
     'file|f=s'   => \my $in_fa,
     'stdin'      => \my $stdin,
+    'rna2dna'    => \my $rna2dna,
 ) or Getopt::Long::HelpMessage(1);
+
+sub SEQ_TR_TU {
+    my $SEQ = shift;
+    return ( $SEQ =~ tr/Uu/Tt/r );
+}
 
 my @info;
 if ( defined($in_fa) ) {
-    open FA, "<", $in_fa or die("$in_fa: $!");
+    open FA, "<", $in_fa;
     @info = <FA>;
     close FA;
 }
@@ -48,7 +47,10 @@ while ( $i <= $#info ) {
     if ( $info[$i] =~ /$char/ ) {
         print( $info[$i] );
         my $j = 1;
-        until (( $i + $j > $#info ) or ( $info[ $i + $j ] =~ /^>/ )) {
+        until ( ( $i + $j > $#info ) or ( $info[ $i + $j ] =~ /^>/ ) ) {
+            if ( defined($rna2dna) ) {
+                $info[ $i + $j ] = SEQ_TR_TU( $info[ $i + $j ] );
+            }
             print( $info[ $i + $j ] );
             $j++;
         }
